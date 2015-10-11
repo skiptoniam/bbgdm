be182de41157b80510aabce505d438c152616e73
skiptoniam <skiptoniam@gmail.com>
2015-10-11 15:55:08 +1100

    normalise function

---
+++ R/normalise.R
@@ -0,0 +1,28 @@
+#' Function to normalise isplines. 
+normalise <- function (x, standardize = "zscore") 
+{
+  if (is.list(standardize)) {
+    stand.a <- standardize$a
+    stand.b <- standardize$b
+  }
+  else if (standardize == "zscore") {
+    stand.a <- mean(x)
+    stand.b <- sd(x)
+    stand.b[stand.b == 0] <- 1
+  }
+  else if (standardize == "interval") {
+    stand.a <- min(x)
+    stand.b <- max(x) - stand.a
+  }
+  else {
+    stand.a <- 0
+    stand.b <- 1
+  }
+  if (is.na(stand.b) || stand.b == 0) {
+    stand.a <- mean(x)
+    stand.b <- 1
+  }
+  X <- (x - stand.a)/stand.b
+  attr(X, "standardization") <- list(a = stand.a, b = stand.b)
+  return(X)
+}
