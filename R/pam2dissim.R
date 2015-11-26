a3c3aea0387bfb83a3530d3ef4b4f2bfa99eaaab
skiptoniam <skiptoniam@gmail.com>
2015-11-26 12:07:57 +1100

    fix - dissim

--- R/pam2dissim.R
+++ R/pam2dissim.R
@@ -67,8 +67,8 @@ pam2dissim <- function(x,dissim="bray_curtis"){
     max_sp <- matrix(NA,nrow(x),nrow(x))
     for(i_site in 1:(nrow(x)-1)){
       for(j_site in c(i_site+1):c(nrow(x))){
-        no_share[i_site,j_site] <- sum(apply(x[c(i_site,j_site),],2,sum)==2)
-#         no_share[i_site,j_site] <- sum(apply(x[c(i_site,j_site),],2,sum)==1)#orig
+#         no_share[i_site,j_site] <- sum(apply(x[c(i_site,j_site),],2,sum)==2)
+        no_share[i_site,j_site] <- sum(apply(x[c(i_site,j_site),],2,sum)==1)#orig
         sum_share[i_site,j_site] <- sum(apply(x[c(i_site,j_site),],2,sum)!=0)
         max_sp[i_site,j_site] <- max(apply(x[c(i_site,j_site),],1,sum))
       }
