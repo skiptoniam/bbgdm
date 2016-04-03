fc01636825401e5ad58102e419263f3716dc27d5
skiptoniam <skiptoniam@gmail.com>
2016-04-03 22:56:04 +1000

    fix plots

--- R/bb_gdm_check.R
+++ R/bb_gdm_check.R
@@ -9,7 +9,7 @@
 #' x <- matrix(rbinom(20*10,1,.6),20,10)# presence absence matrix
 #' y <- simulate_covariates(x,2)
 #' form <- ~ 1 + covar_1 + covar_2
-#' test.bbgdm <- bbgdm(form,sp.dat=x, env.dat=y,family="binomial", dism_metric="number_shared",nboot=10, scale_covar=T)
+#' test.bbgdm <- bbgdm(form,sp.dat=x, env.dat=y,family="binomial", dism_metric="number_non_shared",nboot=10,geo=FALSE, scale_covar=FALSE)
 #' bbgdm.check(test.bbgdm)
 
 bbgdm.check <- function (object, plots.mfrow = c(2, 2),...) 
@@ -19,18 +19,6 @@ bbgdm.check <- function (object, plots.mfrow = c(2, 2),...)
   X <- object$starting_gdm$X
   y <- object$starting_gdm$y
   offset <- object$offset
-  # coefs<-object$starting_gdm$coef
-  # par(mfrow=plots.mfrow)
-  # res <- rqr_function(X,y,coefs,offset)
-  # qqnorm(res)
-  # qqline(res, col = 'red')
-  # hist(res, xlab = "Residuals", main = "Histogram of residuals",...)
-  # lp<- X%*%coefs + offset
-  # pi <- p$linkinv(lp)
-  # plot(pi, res, xlab="Predicted Dissimilarity",ylab="Random Quantile Residuals",cex=.5,pch=16,...)
-  # plot(pi,y[,1]/y[,2], xlab="Predicted Dissimilarity",ylab="Observed Dissimilarity",cex=.5,pch=16,...)
-  # mtext("Original GDM diagnostics",outer = TRUE, cex=1.1,col="black",font=2,line=-1)
-  ## Now for bayesian boot strap estimates
   par(mfrow=plots.mfrow)
   npreds <- object$nboots
    preds<- matrix(0L,npreds,nrow(y))
@@ -39,6 +27,8 @@ bbgdm.check <- function (object, plots.mfrow = c(2, 2),...)
     a <- pbinom(y[,1]-1, y[,2], pi)#-1
     b <- pbinom(y[,1], y[,2], pi)
     u <- runif(n = length(y[,1]), min = a, max = b)
+    u[u==1] <- u[u==1]-1e-5
+    u[u==0] <- u[u==0]+1e-5
     res <- qnorm(u)
     qqnorm(res,ylab='Random Quantile Residuals',main = "")
     qqline(res, col = 'red')
@@ -46,6 +36,5 @@ bbgdm.check <- function (object, plots.mfrow = c(2, 2),...)
     plot(pi,res,cex=.5,pch=16, xlab="Predicted Dissimilarity",ylab="Random Quantile Residuals",...)
     plot(pi,y[,1]/y[,2],cex=.5,pch=16, xlab="Predicted Dissimilarity",ylab="Observed Dissimilarity",...)
     mtext("Bayesian Bootstrap GDM diagnostics",outer = TRUE, cex=1.1,col="black",font=2,line=-1)
-
 }
  
--- R/plot_gdm_bb.R
+++ R/plot_gdm_bb.R
@@ -9,11 +9,11 @@
 #' x <- matrix(rbinom(20*10,1,.6),20,10)# presence absence matrix
 #' y <- simulate_covariates(x,2)
 #' form <- ~ 1 + covar_1 + covar_2
-#' test.bbgdm <- bbgdm(form,sp.dat=x, env.dat=y,family="binomial", dism_metric="number_shared",nboot=10, scale_covar=T)
+#' test.bbgdm <- bbgdm(form,sp.dat=x, env.dat=y,family="binomial", dism_metric="bray_curtis",geo=FALSE,nboot=10, scale_covar=T)
 #' plot(test.bbgdm)
 #' 
 ## plotting
-plot.bbgdm <- function(object,plot.layout = c(2,2),plot.colour='black',plot.linewidth=2,line.col='red'){
+plot.bbgdm <- function(object,plot.layout = c(1,2),plot.colour='black',plot.linewidth=2,line.col='red'){
   if(nrow(object$X)>200) pred_sample <- 200
   else pred_sample <- nrow(object$X)
     par(mfrow=plot.layout)
@@ -21,32 +21,12 @@ plot.bbgdm <- function(object,plot.layout = c(2,2),plot.colour='black',plot.line
     X <- object$X
     Y <- object$starting_gdm$y
     offset <- object$offset
-    gdm.lp <-  X%*%object$starting_gdm$coef + offset
-    gdm.pred <- link.fun$linkinv(gdm.lp)
-    plot(gdm.lp,Y[,1] ,main='starting GDM', xlab = "Predicted Ecological Distance", 
-         ylab = "Observed Non-Shared Species", type = "n")
-    points(gdm.lp,Y[,1] , pch = 20, cex = 0.25,col = plot.colour)
-    y.pred <- Y[sample(pred_sample),]
-    y.pred <- y.pred[order(y.pred[,2], y.pred[,1]),]
-    overlayX.org <- seq(from = min(gdm.lp), to = max(gdm.lp), 
-                    length = pred_sample)
-    overlayY.org <- link.fun$linkinv(overlayX.org)
-    lines(overlayX.org, overlayY.org*y.pred[,2], lwd = plot.linewidth,col=line.col)
-    
-    plot(gdm.lp,Y[,1]/Y[,2] ,main='starting GDM', xlab = "Predicted Ecological Distance", 
-       ylab = "Observed Compositional Dissimilarity", type = "n", ylim=c(0,1))
-    points(gdm.lp,Y[,1]/Y[,2] , pch = 20, cex = 0.25,col = plot.colour)
-    overlayX.org <- seq(from = min(gdm.lp), to = max(gdm.lp), 
-                        length = pred_sample)
-    overlayY.org <- link.fun$linkinv(overlayX.org)
-    lines(overlayX.org, overlayY.org, lwd = plot.linewidth,col=line.col)    
-
     bb.lp <- X%*%object$median.coefs.se + offset
     bb.lp.05 <- X%*%object$quantiles.coefs.se[1,] + offset
     bb.lp.95 <- X%*%object$quantiles.coefs.se[2,] + offset
-    bb.pred <- link.fun$linkinv(bb.lp) #inverse_logit 1/(1+exp(-1*X%*%object$stats.median[1:length(coef(object$starting_gdm))]))
+    bb.pred <- link.fun$linkinv(bb.lp) 
     
-    plot(bb.lp,Y[,1]/Y[,2],main='bb.gdm estimate', xlab = "Linear Predictor", 
+    plot(bb.lp,Y[,1]/Y[,2],main='', xlab = "Linear Predictor", 
          ylab = "Observed Compositional Dissimilarity", type = "n",ylim=c(0,1))
     points(bb.lp,Y[,1]/Y[,2], pch = 20, cex = 0.25, 
            col = plot.colour)
@@ -55,12 +35,10 @@ plot.bbgdm <- function(object,plot.layout = c(2,2),plot.colour='black',plot.line
     overlayX.bb <- seq(from = min(bb.lp), to = max(bb.lp), 
                     length = pred_sample)
     overlayY.bb <- link.fun$linkinv(overlayX.bb)
-#     lines(overlayX, overlayY*y.pred[,2], lwd = plot.linewidth)
     lines(overlayX.bb, overlayY.bb,lwd = plot.linewidth,col=line.col)
-#     lines(overlayX.org, overlayY.org,lwd = plot.linewidth,col="blue")
 
     plot(bb.lp,Y[,1],xlab = "Linear Predictor",ylim=c(0,max(Y[,1])),#xlim=c(-5,5), 
-           ylab = "Predicted non-shared species", type = "n")
+           ylab = "Predicted non_shared species", type = "n")
     points(bb.lp,Y[,1], pch = 20, cex = 0.25, 
              col = plot.colour)
     y.pred <- Y[sample(pred_sample),]
@@ -70,28 +48,4 @@ plot.bbgdm <- function(object,plot.layout = c(2,2),plot.colour='black',plot.line
     overlayY.bb <- link.fun$linkinv(overlayX.bb)
     lines(overlayX.bb, overlayY.bb*y.pred[,2], lwd = plot.linewidth,col=line.col)
     lines(overlayX.org, overlayY.org*y.pred[,2], lwd = plot.linewidth,col="blue")
-    legend("bottomright", c("BBGDM mean","Single GDM mean"), pch = 16,col = c("red", "blue"))    
-    
-    y.est.runs <- matrix(NA,nrow(X),object$nboots)
-    lp.runs <- matrix(NA,nrow(X),object$nboots)
-    for(ii in 1:object$nboots){
-      lp <- X%*%object$bb_gdms[[ii]]$coef
-      link.fun$linkinv(lp)
-      y.est.runs[,ii] <- link.fun$linkinv(lp)
-      lp.runs[,ii]<-lp
-    }
-    bb.lp <- X%*%object$median.coefs.se + offset
-    quantiles.bb.lp.05 <- X%*%object$quantiles.coefs.se[1,] + offset
-    quantiles.bb.lp.95 <- X%*%object$quantiles.coefs.se[2,] + offset
-    
-    yhat_mean <- link.fun$linkinv(bb.lp) #inverse_logit 1/(1+exp(-1*X%*%object$stats.median[1:length(coef(object$starting_gdm))]))
-    yhat_q05 <- link.fun$linkinv(quantiles.bb.lp.05)
-    yhat_q95 <- link.fun$linkinv(quantiles.bb.lp.95)
-    
-#     plot(-1,ylab='Predicted Compositional Dissimilarity',xlab='Observed Compositional Dissimilarity',ylim=range(Y[,1]),xlim=range(c(yhat_mean*Y[,2],yhat_q05*Y[,2],yhat_q95*Y[,2])),type = "n")
-#     points(yhat_q05*Y[,2], Y[,1], pch=16, cex=.5,col='blue')
-#     points(yhat_q95*Y[,2], Y[,1], pch=16, cex=.5,,col='red')
-#     points(yhat_mean*Y[,2], Y[,1], pch=16, cex=.5,col = "orange")
-#     legend("bottomright", c("5% quantile","mean","95% quantile"), pch = 16,col = c("blue", "orange", "red"))    
-
 }
