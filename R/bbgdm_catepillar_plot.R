20f9106572ebb8a1704d5ef2a45b8127d4f09cff
skiptoniam <skiptoniam@gmail.com>
2016-04-09 16:31:35 +1000

    added caterpillar plot for bbgdm objects.

---
+++ R/bbgdm_catepillar_plot.R
@@ -0,0 +1,83 @@
+#' bbgdm catepillar plots function
+#' @param object a \code{bbgdm} object
+#' @param pars name of parameters to plot.
+#' @param pars_labels vector of parameter labels.
+#' @param zero_line logical. Whether or not to the zero #' medians.
+#' @param horizontal logical. Whether or not you would like the lines to be
+#' horizontal
+
+bbgdm_caterpillar_plot <- function(object, pars=NULL, pars_labels = NULL,
+                              horizontal = TRUE, 
+                              zero_line=TRUE,
+                              xlims = c(-1,1)){
+  # Load packages
+  library(tidyr)
+  library(dplyr)
+  library(ggplot2)
+  library(ggthemes)
+  
+  # Extract all simulations
+  sims <- as.data.frame(object$all.coefs.se)
+  
+  # Extract only desired parameters
+  par_names <- names(sims)
+  if(!is.null(pars)){
+    sims_subset <- sims
+    cat('plotting all spline bases \n')
+  } else {
+  sims_subset <- sims[, par_names %in% pars]
+  }
+  if (ncol(sims_subset) == 0) {
+    stop("No parameters selected. \n", call. = FALSE)
+  }
+  # Gather for plotting
+  gathered <- gather(sims_subset, variable, value)
+  
+  # Add labels
+  if (!is.null(pars_labels)) {
+    message("\nEnsure that your parameter labels are in the same order as the parameters.\n")
+    if (length(pars_labels) !=
+        length(unique(gathered$variable))) {
+      stop("pars_labels must equal the number of plotted parameters.",
+           call. = FALSE)
+    }
+    gathered$variable <- factor(gathered$variable,
+                                labels = pars_labels)
+  }
+  
+  # Find central interval
+    gathered <- group_by(gathered, variable)
+    lower95 <- summarise(gathered, quantile(value, 0.025))
+    lower90 <- summarise(gathered, quantile(value, 0.05))
+    upper90 <- summarise(gathered, quantile(value, 0.95))
+    upper95 <- summarise(gathered, quantile(value, 0.975))
+  
+  # Find medians
+  medians <- summarise(gathered, median(value))
+  
+  # Merge
+  comb <- suppressMessages(inner_join(lower95, lower90))
+  comb <- suppressMessages(inner_join(comb, medians))
+  comb <- suppressMessages(inner_join(comb, upper90))
+  comb <- suppressMessages(inner_join(comb, upper95))
+  names(comb) <- c('pars', 'lower95', 'lower90', 'medians', 'upper90',
+                   'upper95')
+  
+  # Plot
+  pp <- ggplot(comb, aes(x = medians, y = pars,
+                           xmin = lower95,
+                           xmax = upper95)) +
+      geom_point(size = 3) +
+      geom_segment(aes(x = lower95, xend = upper95, yend = pars),
+                   size = 0.5) +
+      geom_segment(aes(x = lower90, xend = upper90,
+                       yend = pars), size = 1.5) +
+      xlab('') + ylab('') +
+      xlim(xlims)+
+      theme_few()+
+      theme(plot.margin=unit(c(5,5,5,5),"mm"))
+  
+  if (isTRUE(zero_line)) pp <- pp + geom_vline(xintercept = 0)
+  if (!isTRUE(horizontal)) pp <- pp + coord_flip()
+  return(pp)
+}
