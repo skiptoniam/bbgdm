487619ad9e9dbc11306844b4508e4eceeb6687c5
skiptoniam <skiptoniam@gmail.com>
2016-06-29 10:38:14 +1000

    remove par options from plot bbgdm

--- R/plot_gdm_bb.R
+++ R/plot_gdm_bb.R
@@ -18,10 +18,10 @@
 #' plot(test.bbgdm)
 #'
 ## plotting
-plot.bbgdm <- function(object,plot.layout = c(1,1),plot.colour='black',plot.linewidth=2,line.col='red',...){
+plot.bbgdm <- function(object,plot.colour='black',plot.linewidth=2,line.col='red',...){
   if(nrow(object$X)>200) pred_sample <- 200
   else pred_sample <- nrow(object$X)
-    par(mfrow=plot.layout,oma = c(0, 0, 2, 0))
+    # par(mfrow=plot.layout,oma = c(0, 0, 2, 0))
     link <-object$link
     if(link=='negexp') link.fun <- bbgdm::negexp()
     else link.fun <- make.link(link=link)
@@ -32,7 +32,7 @@ plot.bbgdm <- function(object,plot.layout = c(1,1),plot.colour='black',plot.line
     bb.eta.05 <- X%*%object$quantiles.coefs.se[1,] + offset
     bb.eta.95 <- X%*%object$quantiles.coefs.se[2,] + offset
     bb.pred <- link.fun$linkinv(bb.eta)
-    plot(bb.eta,Y[,1]/Y[,2],main="Observed compositional dissimilarity vs. the linear predictor", xlab = "Linear Predictor",
+    plot(bb.eta,Y[,1]/Y[,2], xlab = "Linear Predictor",
          ylab = paste0("Observed ",object$dism_metric," dissimilarity"), type = "n",ylim=c(0,1),...)
     points(bb.eta,Y[,1]/Y[,2], pch = 20, cex = 0.25,
            col = plot.colour)
